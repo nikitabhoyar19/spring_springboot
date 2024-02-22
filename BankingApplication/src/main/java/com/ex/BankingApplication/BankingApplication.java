352eb2c55616c1781ae1e60414ddc9d12968f133
Nikita Bhoyar <nikitambhoyar@gmail.com>
2024-02-22 10:05:52 +0530

    added method security and used db users

--- BankingApplication/src/main/java/com/ex/BankingApplication/BankingApplication.java
+++ BankingApplication/src/main/java/com/ex/BankingApplication/BankingApplication.java
@@ -8,7 +8,7 @@ public class BankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplication.class, args);
-		System.out.println("It is Bank Application");
+		System.out.println("Bank Application");
 	}
 
 }
